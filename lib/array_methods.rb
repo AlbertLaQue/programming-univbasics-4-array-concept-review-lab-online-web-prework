9d0c66f239aae06ca076be1fd842ad17ae25c6a3
Albert LaQue <albertlaque@gmail.com>
2019-10-27 23:59:52 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -21,11 +21,11 @@ x = 0
   end
 
 def find_min_value(array)
-x = [0] 
-  array.length.times do |index|
-  if array[index] < x 
-  x = array[index]
-  end
-  end
-  x
-end
+ x = array [0]
+ array.length.times do |index|
+   if array[index] < x 
+     x = array[index]
+   end
+ end
+ x 
+ end
